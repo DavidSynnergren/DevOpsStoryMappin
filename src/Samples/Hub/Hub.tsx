610448fd5a9064bc889f88146e952986250912bf
David Synnergren <david.synnergren@hogia.se>
2019-05-09 22:51:35 +0200

    added react and a bunch of mumbo jumbo

---
+++ src/Samples/Hub/Hub.tsx
@@ -0,0 +1,45 @@
+import "./Hub.scss";
+
+import * as React from "react";
+import * as SDK from "azure-devops-extension-sdk";
+
+import { Page } from "azure-devops-ui/Page";
+
+import { showRootComponent } from "../../Common";
+
+interface IHubContentState {
+    selectedTabId: string;
+    fullScreenMode: boolean;
+    headerDescription?: string;
+    useLargeTitle?: boolean;
+    useCompactPivots?: boolean;
+}
+
+class HubContent extends React.Component<{}, IHubContentState> {
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            selectedTabId: "overview",
+            fullScreenMode: false
+        };
+    }
+
+    public componentDidMount() {
+        SDK.init();
+    }
+
+    public render(): JSX.Element {
+
+        const { selectedTabId, headerDescription, useCompactPivots, useLargeTitle } = this.state;
+
+        return (
+            <Page className="sample-hub flex-grow">                    
+                <p>Hejsan hoppsan!</p>
+            </Page>
+        );
+    }
+}
+
+showRootComponent(<HubContent />);
