5f603ac505063f687f99d56df67694cfeac4d7dc
David Synnergren <david.synnergren@hogia.se>
2019-05-09 23:47:16 +0200

    added rows for releases... VERY MUCH IN PROGRESS

--- src/Samples/Hub/Hub.tsx
+++ src/Samples/Hub/Hub.tsx
@@ -5,6 +5,8 @@ import * as SDK from "azure-devops-extension-sdk";
 
 import { Page } from "azure-devops-ui/Page";
 import { Header, TitleSize } from "azure-devops-ui/Header";
+import { Card } from "azure-devops-ui/Card";
+import { SplitterElementPosition, Splitter } from "azure-devops-ui/Splitter";
 
 import { showRootComponent } from "../../Common";
 
@@ -40,8 +42,40 @@ class HubContent extends React.Component<{}, IHubContentState> {
                 <Header
                     title="Story Mappin'"
                     titleSize={useLargeTitle ? TitleSize.Large : TitleSize.Medium} />            
-                <p>Hejsan hoppsan!</p>
+                <Card>
+                    <Splitter
+                        fixedElement={SplitterElementPosition.Near}
+                        splitterDirection={0}
+                        initialFixedSize={300}
+                        minFixedSize={100}
+                        nearElementClassName="v-scroll-auto custom-scrollbar"
+                        farElementClassName="v-scroll-auto custom-scrollbar"
+                        onRenderNearElement={this._renderNearContent}
+                        onRenderFarElement={this._renderFarContent} />  
+                </Card>
             </Page>
+        );
+    }
+
+    private _renderNearContent(): JSX.Element {
+        return (
+            <div style={{ padding: "16px" }}>
+                <Header
+                    title="Release 1"
+                    titleSize={TitleSize.Small} />   
+                <p>Lägg kort här typ...</p>
+            </div>
+        );
+    }
+
+    private _renderFarContent(): JSX.Element {
+        return (
+            <div style={{ padding: "16px" }}>
+                <Header
+                    title="Release 2"
+                    titleSize={TitleSize.Small} />   
+                <p>... och några här kanske.</p>
+            </div>
         );
     }
 }
